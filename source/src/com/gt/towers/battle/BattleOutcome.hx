0b2875ee9845e71fdaf3cc05b8886690d2f5f6e0
Mansour Djawadi <mansurjavadi@gmail.com>
2017-06-17 12:29:49 +0430

    new outcome formula

--- source/src/com/gt/towers/battle/BattleOutcome.hx
+++ source/src/com/gt/towers/battle/BattleOutcome.hx
@@ -1,9 +1,9 @@
 package com.gt.towers.battle;
+import com.gt.towers.Game;
 import com.gt.towers.Player;
+import com.gt.towers.battle.fieldes.FieldData;
 import com.gt.towers.constants.ResourceType;
 import com.gt.towers.utils.maps.Bundle;
-import com.gt.towers.utils.maps.IntIntMap;
-import com.gt.towers.Game;
 
 /**
  * ...
@@ -12,43 +12,59 @@ import com.gt.towers.Game;
 class BattleOutcome 
 {
 
+	public function new(){}
+	
+	#if java
+
+	public static var MAX_XP:Int = 10;
 	public static var MAX_POINTS:Int = 30;
 	public static var MAX_CARDS:Int = 2;
 	
-	public function new(){}
 	
-	public function get_outcomes(winner:Bool):Bundle
+	public static function get_outcomes(player:Player, field:FieldData, score:Int):Bundle
 	{
 		var ret:Bundle = new Bundle();
-		var pl:Player = Game.get_instance().get_player();
 		
-		// calculate money
-		ret.set(ResourceType.CURRENCY_0, pl.get_arena() * 10);
 		
-		// calculate point
-		if (pl.get_point() <  MAX_POINTS && !winner)
-			ret.set(ResourceType.POINT, 0 );
-		else
-			ret.set(ResourceType.POINT, MAX_POINTS * (winner ? 1 : -1) );
 		
-		// calculate cards
-		/*var unlockedWeapons = Game.get_instance().get_player().get_buildings().get_availables();
-		var i:Int = 0;
-		while (i < unlockedWeapons.length)
+		var rewardChance : Int = 0;
+		if ( field.isQuest )
 		{
-			if (Math.random() < 0.1)
-				ret.set(unlockedWeapons[i], Math.ceil(Math.random() * MAX_CARDS));
+			if ( !player.get_quests().exists(field.index) || player.get_quests().get(field.index) < score )
+				rewardChance = score - player.get_quests().get(field.index);
+			
+			// calculate xp
+			ret.set(ResourceType.XP, cast( Math.max(0, MAX_XP * (score > 0 ? score : -3) / 3 ), Int ) );
+			
+			// calculate money
+			ret.set(ResourceType.CURRENCY_0, 10 * score);
+		}
+		else
+		{
+			rewardChance = score;
+			
+			// calculate point
+			ret.set(ResourceType.POINT, cast( Math.max(0, MAX_POINTS * (score > 0 ? score : -3) / 3 ), Int ) );
+		}
+		
+		
+		// calculate rewards
+		var unlockedBuildings = player.get_buildingsLevel().keys();
+		var i:Int = 0;
+		var numUnlocked = Math.min(unlockedBuildings.length, rewardChance);
+		while (i < numUnlocked)
+		{
+			//if ( Math.random() < 1/numUnlocked )
+				ret.set(unlockedBuildings[i], Math.ceil(Math.random() * MAX_CARDS));
 			i++;
-		}*/
+		}
 
 		return ret;
 	}
+	#end
 	
-	public function consume_outcomes(outcomes:Bundle):Void
+	public static function consume_outcomes(player:Player, outcomes:Bundle):Void
 	{
-		Game.get_instance().get_player().get_resources().increaseMap(outcomes);
+		player.get_resources().increaseMap(outcomes);
 	}
-
-
-	
 }
