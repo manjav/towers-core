d7afe2fba037d509ae83c7f3eb68d27b88bad242
babak-ss <ssalimib@gmail.com>
2017-07-22 00:28:26 +0430

    building feature balance

--- source/src/com/gt/towers/Game.hx
+++ source/src/com/gt/towers/Game.hx
@@ -42,15 +42,15 @@ class Game
 		fieldProvider = new FieldProvider();
 		
 		arenas = new IntArenaMap();
-		arenas.set(0, new Arena(0, 0,		30,		"11"	));
-		arenas.set(1, new Arena(1, 31,		100,	"12"	));
-		arenas.set(2, new Arena(2, 101,		250,	"21,31"	));
-		arenas.set(3, new Arena(3, 251,		600,	"13,22"	));
-		arenas.set(4, new Arena(4, 601,		1000,	"41,32"	));
-		arenas.set(5, new Arena(5, 1001,	1500,	"42,23"	));
-		arenas.set(6, new Arena(6, 1501,	2200,	"14,33"	));
-		arenas.set(7, new Arena(7, 2201,	3000,	"43,24"	));
-		arenas.set(8, new Arena(8, 3001,	100000,	"44,34"	));
+		arenas.set(0, new Arena(0, 0,		20,		"21"    ));
+		arenas.set(1, new Arena(1, 21,		60,		"31"	));
+		arenas.set(2, new Arena(2, 61,		135,	"12"	));
+		arenas.set(3, new Arena(3, 136,		240,	"41"	));
+		arenas.set(4, new Arena(4, 240,		400,	"32"	));
+		arenas.set(5, new Arena(5, 401,		650,	"22,42"	));
+		arenas.set(6, new Arena(6, 651,		1100,	"13,23"	));
+		arenas.set(7, new Arena(7, 981,		1900,	"33,43"	));
+		arenas.set(8, new Arena(8, 1901,	20000,	"24,14"	));
 	}
 	
 	public function unlockedBuildingAt ( type:Int ) : Int
