0af52e5f1b99d0a3039952839fe10a1421b6d0cb
ManJav <mansurjavadi@gmail.com>
2017-09-16 19:02:44 +0430

    version up

--- source/src/com/gt/towers/LoginData.hx
+++ source/src/com/gt/towers/LoginData.hx
@@ -15,7 +15,7 @@ class LoginData
 {
 	
 	public static var coreSize:Int = 0;
-	public var coreVersion:String = "1061.0908205132";//do not change len.
+	public var coreVersion:String = "1062.0916171112";//do not change len.
 	public var noticeVersion:Int = 1060;
 	public var forceVersion:Int = 150;
 	public var resources:IntIntMap;
