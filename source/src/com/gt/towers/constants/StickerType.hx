c0484670e5414986be7aff06c929a29ceba55996
Babak <ssalimib@gmail.com>
2017-09-23 16:15:08 +0330

    upgrade quest_0 building new sticker type

--- source/src/com/gt/towers/constants/StickerType.hx
+++ source/src/com/gt/towers/constants/StickerType.hx
@@ -12,10 +12,10 @@ class StickerType
 	
 	public static var S0_LUCKKY:Int = 0;
 	public static var S1_THANKS:Int = 1;
-	public static var S2_WELL_PLAYED:Int = 2;
-	public static var S3_GOOD_GAME:Int = 3;
+	public static var S2_THREAT:Int = 2;
+	public static var S3_WELL_PLAYED:Int = 3;
 	public static var S4_OOPS:Int = 4;
-	public static var S5_THISIS:Int = 5;
+	public static var S5_ALL_YOU_GOT:Int = 5;
 	public static var S6_HAHAHA:Int = 6;
 	public static var S7_REVENGE:Int = 7;
 	public static var S8_CURSED:Int = 8;
@@ -26,10 +26,10 @@ class StickerType
 		
 		ret.push( S0_LUCKKY );
 		ret.push( S1_THANKS );
-		ret.push( S2_WELL_PLAYED );
-		ret.push( S3_GOOD_GAME );
+		ret.push( S2_THREAT );
+		ret.push( S3_WELL_PLAYED );
 		ret.push( S4_OOPS );
-		ret.push( S5_THISIS );
+		ret.push( S5_ALL_YOU_GOT );
 		ret.push( S6_HAHAHA );
 		ret.push( game.player.inFriendlyBattle ? S7_REVENGE : S8_CURSED );
 		
@@ -41,12 +41,14 @@ class StickerType
 	{
 		var ret:IntList = new IntList();
 		
-		if ( sticker == S2_WELL_PLAYED  || sticker == S3_GOOD_GAME )
+		if ( sticker == S3_WELL_PLAYED )
 			ret.push( S1_THANKS );
-		else if ( sticker == S0_LUCKKY || sticker == S4_OOPS || sticker == S7_REVENGE )
+		else if ( sticker == S0_LUCKKY || sticker == S4_OOPS || sticker == S7_REVENGE || sticker == S8_CURSED)
 			ret.push( S6_HAHAHA );
-		else if ( sticker == S5_THISIS )
+		else if ( sticker == S6_HAHAHA )
 			ret.push( S0_LUCKKY );
+		else if ( sticker == S5_ALL_YOU_GOT )
+			ret.push( S2_THREAT );
 			
 		return ret;
 	}
