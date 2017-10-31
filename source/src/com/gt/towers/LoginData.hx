1d34e370b47bba3bbc485baef4d3abd4c56dda69
ManJav <mansurjavadi@gmail.com>
2017-10-31 13:07:09 +0330

    fixed purchase bugs

--- source/src/com/gt/towers/LoginData.hx
+++ source/src/com/gt/towers/LoginData.hx
@@ -13,9 +13,8 @@ import com.gt.towers.utils.maps.IntIntMap;
  */
 class LoginData
 {
-	
 	public static var coreSize:Int = 0;
-	public var coreVersion:String = "2018.1030154846";//do not change len.
+	public var coreVersion:String = "2018.1031125024";//do not change len.
 	public var noticeVersion:Int = 2015;
 	public var forceVersion:Int = 2005;
 	public var resources:IntIntMap;
