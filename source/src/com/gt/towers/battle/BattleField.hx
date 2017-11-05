3f30d3231034a3aa4b0ab6c606d95fcf197a36d6
ManJav <mansurjavadi@gmail.com>
2017-11-05 12:30:40 +0330

    fixed battlefield bug return to old quest_1 args

--- source/src/com/gt/towers/battle/BattleField.hx
+++ source/src/com/gt/towers/battle/BattleField.hx
@@ -46,7 +46,7 @@ class BattleField
 		{
 			if ( isQuest )
 			{
-				if ( map.index == 1 && game_0.player.buildings.get(BuildingType.B11_BARRACKS).get_level() < 2 )
+				if ( map.index == 1 && game_0.player.buildings.exists(BuildingType.B11_BARRACKS) &&  game_0.player.buildings.get(BuildingType.B11_BARRACKS) .get_level() <= 1 )
 					difficulty = 4;
 				else
 					difficulty = Math.round(map.index / 6);
