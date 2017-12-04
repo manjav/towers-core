788a7639ffe1bde1adf80dd8efd1e32a80f6b4b5
ManJav <mansurjavadi@gmail.com>
2017-12-05 02:01:45 +0330

    better changables added some card types

--- source/src/com/gt/towers/constants/BuildingFeatureType.hx
+++ source/src/com/gt/towers/constants/BuildingFeatureType.hx
@@ -1,4 +1,5 @@
 package com.gt.towers.constants;
+import com.gt.towers.Game;
 import com.gt.towers.utils.lists.IntList;
 import com.gt.towers.utils.maps.IntIntMap;
 
@@ -25,6 +26,7 @@ class BuildingFeatureType
 	public static var F24_RANGE_RANGE_MAX:Int = 24;
 	public static var F25_DAMAGE_AREA:Int = 25;
 	
+#if flash
 	private static var _all:IntList;
 	public static function getAll():IntList
 	{
@@ -38,6 +40,7 @@ class BuildingFeatureType
 			_all.push( F11_TROOP_SPEED );
 			_all.push( F12_TROOP_POWER );
 			_all.push( F13_TROOP_HEALTH );
+			//_all.push( F14_TROOP_RUSH_GAP );
 			
 			_all.push( F21_DAMAGE );
 			_all.push( F22_DAMAGE_GAP );
@@ -57,7 +60,7 @@ class BuildingFeatureType
 		ret.push( F11_TROOP_SPEED );
 		ret.push( F12_TROOP_POWER );
 		ret.push( F13_TROOP_HEALTH );
-		if ( buildingType > BuildingType.B40_CRYSTAL )
+		if ( buildingType >= CardTypes.C500 )
 		{
 			ret.push( F21_DAMAGE );
 			ret.push( F22_DAMAGE_GAP );
@@ -66,27 +69,18 @@ class BuildingFeatureType
 		return ret;
 	}
 	
-	public static function getChangables(buildingType:Int):IntList
+	public static function getChangables(game:Game, type:Int, level:Int):IntList
 	{
 		var ret = new IntList();
-		var cate = BuildingType.get_category(buildingType);
-		if ( cate == BuildingType.B10_BARRACKS )
-			ret.push( F02_BIRTH_RATE );
-		else if ( cate == BuildingType.B20_RAPID )
+		var all = getAll();
+		var i = 0;
+		while ( i < all.size() )
 		{
-			ret.push( F11_TROOP_SPEED );
-			ret.push( F12_TROOP_POWER );
-		}
-		else if ( cate == BuildingType.B30_HEAVY )
-		{
-			ret.push( F12_TROOP_POWER );
-		}
-		else if ( cate == BuildingType.B40_CRYSTAL )
-		{
-			ret.push( F21_DAMAGE );
-			ret.push( F22_DAMAGE_GAP );
-			ret.push( F24_RANGE_RANGE_MAX );
+			if ( game.featureCaculator.get(all.get(i), type, level) != game.featureCaculator.get(all.get(i), type, level + 1) )
+				ret.push(all.get(i));
+			i ++;
 		}
 		return ret;
 	}
+#end
 }
--- source/src/com/gt/towers/constants/CardTypes.hx
+++ source/src/com/gt/towers/constants/CardTypes.hx
@@ -8,6 +8,7 @@ class CardTypes
 {
 	public static var C001:Int = 1;
 	
+	public static var C100:Int = 100;
 	public static var C101:Int = 101;
 	public static var C102:Int = 102;
 	public static var C103:Int = 103;
@@ -18,6 +19,7 @@ class CardTypes
 	public static var C108:Int = 108;
 	public static var C109:Int = 109;
 	
+	public static var C200:Int = 200;
 	public static var C201:Int = 201;
 	public static var C202:Int = 202;
 	public static var C203:Int = 203;
@@ -28,6 +30,7 @@ class CardTypes
 	public static var C208:Int = 208;
 	public static var C209:Int = 209;
 	
+	public static var C300:Int = 300;
 	public static var C301:Int = 301;
 	public static var C302:Int = 302;
 	public static var C303:Int = 303;
@@ -38,6 +41,7 @@ class CardTypes
 	public static var C308:Int = 308;
 	public static var C309:Int = 309;
 	
+	public static var C400:Int = 400;
 	public static var C401:Int = 401;
 	public static var C402:Int = 402;
 	public static var C403:Int = 403;
@@ -47,6 +51,11 @@ class CardTypes
 	public static var C407:Int = 407;
 	public static var C408:Int = 408;
 	public static var C409:Int = 409;
+	
+	public static var C500:Int = 500;
+	public static var C501:Int = 501;
+	public static var C502:Int = 502;
+	public static var C503:Int = 503;
 	
 	public function new() { }
 	
