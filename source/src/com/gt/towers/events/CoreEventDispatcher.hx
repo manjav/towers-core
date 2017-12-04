5f67fa928bb20dc5ffa0361761c2703c0160f006
ManJav <mansurjavadi@gmail.com>
2017-12-05 02:02:00 +0330

    no message

---
+++ source/src/com/gt/towers/events/CoreEventDispatcher.hx
@@ -0,0 +1,42 @@
+package com.gt.towers.events;
+
+#if java
+	import com.gt.towers.utils.ValueChangeCallback;
+#elseif flash
+	import flash.events.EventDispatcher;
+	import com.gt.towers.events.CoreEvent;
+#end
+
+/**
+ * ...
+ * @author Mansour Djawadi
+ */
+#if java
+class CoreEventDispatcher
+#elseif flash
+class CoreEventDispatcher extends EventDispatcher
+#end
+{
+#if java
+	public var changeCallback:ValueChangeCallback;
+#end
+	
+	public function new() 
+	{
+#if flash
+		super();
+#end
+	}
+	
+	public function dispatch(key:Dynamic, from:Dynamic, to:Dynamic) 
+	{
+#if java
+		if ( changeCallback != null )
+			changeCallback.update( key, from, to );
+#elseif flash
+		//if( hasEventListener ( CoreEvent.CHANGE ) )
+			dispatchEvent(new CoreEvent(CoreEvent.CHANGE, key, from, to) );
+#end
+	}
+	
+}
