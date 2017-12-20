f7accf30b27c3b5a6e6d8f3f00aae1db2d6ae6bb
ManJav <mansurjavadi@gmail.com>
2017-12-20 19:12:24 +0330

    version up

--- source/src/com/gt/towers/LoginData.hx
+++ source/src/com/gt/towers/LoginData.hx
@@ -14,8 +14,8 @@ import com.gt.towers.utils.maps.IntIntMap;
 class LoginData
 {
 	public static var coreSize:Int = 0;
-	public var coreVersion:String = "2050.1218172038";//do not change len.
-	public var noticeVersion:Int = 2050;
+	public var coreVersion:String = "2060.1219164404";//do not change len.
+	public var noticeVersion:Int = 2060;
 	public var forceVersion:Int = 2050;
 	public var resources:IntIntMap;
 	public var buildingsLevel:IntIntMap;
