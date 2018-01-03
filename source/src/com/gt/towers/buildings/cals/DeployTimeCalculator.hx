c47d76024e13492550be8afe5767f6eca8a49869
Babak <ssalimib@gmail.com>
2018-01-03 14:45:10 +0330

    Added deploy time

--- source/src/com/gt/towers/buildings/cals/DeployTimeCalculator.hx
+++ source/src/com/gt/towers/buildings/cals/DeployTimeCalculator.hx
@@ -11,6 +11,18 @@ class DeployTimeCalculator
 	public function new () {}
 	public function get(type:Int, level:Int):Float 
 	{
+		if ( CardTypes.get_category( type ) == CardTypes.C200 )
+			return  BASE_VALUE + Math.log(level) * 0.07;
+			
+		if ( CardTypes.get_category( type ) == CardTypes.C300 )
+			return  BASE_VALUE + Math.log(level) * 0.025 - 0.2 ;
+			
+		if ( CardTypes.get_category( type ) == CardTypes.C400 )
+			return  BASE_VALUE + Math.log(level) * 0.05 ;
+			
+		if ( CardTypes.get_category( type ) == CardTypes.C500 )
+			return  BASE_VALUE + Math.log(level) * 0.2;
+			
 		return  BASE_VALUE + Math.log(level) * 0.05 ;
 	}
 }
