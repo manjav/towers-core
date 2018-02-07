7808b818a1c3c08e67d8f0c4d6a16d02b59a19db
ManJav <mansurjavadi@gmail.com>
2018-02-07 21:25:18 +0330

    fixed get arena bug

--- source/src/com/gt/towers/Player.hx
+++ source/src/com/gt/towers/Player.hx
@@ -128,20 +128,20 @@ class Player
 		}
 	}
 	
-	public function get_arena(point:Int):Int
+	public function get_arena(point:Int) : Int
 	{
 		if( point == 0 )
 			point = get_point();
 			
 		var arenaKeys = game.arenas.keys();
 		var arena = arenaKeys.length - 1;
-		while ( arena >= 0)
+		while ( arena >= 0 )
 		{
-			if( point > game.arenas.get( arenaKeys[arena] ).max )
-				return arena;
+			if ( point > game.arenas.get( arenaKeys[arena] ).max )
+				break;
 			arena --;
 		}
-		return 0 ;
+		return cast(Math.min(arena + 1, arenaKeys.length-1), Int);
 	}
 
 	public function availabledCards (selectedArena:Int = -1) : IntList
@@ -166,7 +166,7 @@ class Player
 		return decks.get(selectedDeck);
 	}
 	
-	public function has(IntIntMap:IntIntMap):Bool
+	public function has(IntIntMap:IntIntMap) : Bool
 	{
 		var i:Int = 0;
 		var keis = IntIntMap.keys();
