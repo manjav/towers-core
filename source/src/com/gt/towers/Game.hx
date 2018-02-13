eeaa5082ac4d366f9e9bfbdb06486596f53056a0
ManJav <mansurjavadi@gmail.com>
2018-02-14 00:36:49 +0330

    min streaks and buildings level balanced

--- source/src/com/gt/towers/Game.hx
+++ source/src/com/gt/towers/Game.hx
@@ -3,17 +3,12 @@ import com.gt.towers.InitData;
 import com.gt.towers.Player;
 import com.gt.towers.arenas.Arena;
 import com.gt.towers.battle.FieldProvider;
-import com.gt.towers.buildings.Building;
 import com.gt.towers.constants.BuildingType;
 import com.gt.towers.exchanges.Exchanger;
 import com.gt.towers.socials.Lobby;
 import com.gt.towers.utils.maps.IntArenaMap;
 import com.gt.towers.utils.maps.IntBuildingMap;
-import com.gt.towers.utils.maps.IntIntMap;
 
-#if java
-import com.gt.towers.battle.AIEnemy;
-#end
 
 /**
  * ...
@@ -49,16 +44,16 @@ class Game
 		arenas = new IntArenaMap();
 		
 	  //				Arena(num, min,		max,	minWinStreak,	unlockCards));
-		arenas.set(0, new Arena(0, 0,		20,		-4,				"11"    ));
-		arenas.set(1, new Arena(1, 21,		100,	-3,				"21,31"	));
-		arenas.set(2, new Arena(2, 101,		200,	-2,				"41,12"	));
-		arenas.set(3, new Arena(3, 201,		300,	-1,				"13,22"	));
-		arenas.set(4, new Arena(4, 301,		500,	 0,				"32,42" ));
-		arenas.set(5, new Arena(5, 501,		900,	 1,				"23,33"	));
-		arenas.set(6, new Arena(6, 901,		1500,	 2,				"14,43"	));
-		arenas.set(7, new Arena(7, 1501,	2500,	 3,				"24"	));
-		arenas.set(8, new Arena(8, 2501,	4000,	 4, 			"34"	));
-		arenas.set(9, new Arena(9, 4001,	10000,	 5, 			"44"	));
+		arenas.set(0, new Arena(0, 0,		20,		-9,				"11"    ));
+		arenas.set(1, new Arena(1, 21,		100,	-8,				"21,31"	));
+		arenas.set(2, new Arena(2, 101,		200,	-7,				"41,12"	));
+		arenas.set(3, new Arena(3, 201,		300,	-6,				"13,22"	));
+		arenas.set(4, new Arena(4, 301,		500,	-5,				"32,42" ));
+		arenas.set(5, new Arena(5, 501,		900,	-4,				"23,33"	));
+		arenas.set(6, new Arena(6, 901,		1500,	-3,				"14,43"	));
+		arenas.set(7, new Arena(7, 1501,	2500,	-2,				"24"	));
+		arenas.set(8, new Arena(8, 2501,	4000,	-1, 			"34"	));
+		arenas.set(9, new Arena(9, 4001,	10000,	 0, 			"44"	));
 		
 		fieldProvider = new FieldProvider(this);
 	}
@@ -84,7 +79,7 @@ class Game
 	
 	public function fillAllBuildings() : Void
 	{
-		var baseLevel:Int = Math.round(Math.max(1, Math.log(player.get_point() / 250) * 4) + 2);
+		var baseLevel:Int = Math.round(Math.max(1, Math.log(player.get_point() / 250) * 2));
 		var arenaIndex = 0;
 		player.buildings = new IntBuildingMap();
 		while ( arenaIndex < arenas.keys().length )
@@ -98,7 +93,7 @@ class Game
 			{
 				var type = arena.cards.get(cardsLen);
 				var level = baseLevel + 4 - BuildingType.get_improve(type);
-				trace("bot building " + type + ", level: " + level );
+				trace("bot building " + type, " level:" + level);
 				player.buildings.set(type, BuildingType.instantiate( this, type, null, 0, level ) );
 				cardsLen --;
 			}
