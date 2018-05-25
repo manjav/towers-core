c9be0cd89dee5e5ed6fd5ffaf46d858d14b80e2a
ManJav <mansurjavadi@gmail.com>
2018-05-26 02:56:40 +0430

    removed extra stickers

--- source/src/com/gt/towers/constants/StickerType.hx
+++ source/src/com/gt/towers/constants/StickerType.hx
@@ -13,17 +13,17 @@ class StickerType
 	public static var S1_THANKS:Int = 1;
 	public static var S2_THREAT:Int = 2;
 	public static var S3_WELL_PLAYED:Int = 3;
-	public static var S4_OOPS:Int = 4;
-	public static var S5_ALL_YOU_GOT:Int = 5;
+	//public static var S4_OOPS:Int = 4;
+	//public static var S5_ALL_YOU_GOT:Int = 5;
 	public static var S6_HAHAHA:Int = 6;
 	public static var S7_REVENGE:Int = 7;
 	public static var S8_CURSED:Int = 8;
 	
-	public static var S9_I_AFRAID:Int = 9;
+	//public static var S9_I_AFRAID:Int = 9;
 	public static var S10_DO_YOU_ANY:Int = 10;
 	public static var S11_NOVICE:Int = 11;
 	public static var S12_PRACTICE:Int = 12;
-	public static var S13_DISCONNECTED:Int = 13;
+	//public static var S13_DISCONNECTED:Int = 13;
 	public static var S14_PROFESSIONAL:Int = 14;
 	public static var S15_DONT_FORCE:Int = 15;
 	public static var S16_I_SEE_YOU_SMALL:Int = 16;
@@ -37,18 +37,18 @@ class StickerType
 		ret.push( S1_THANKS );
 		ret.push( S2_THREAT );
 		ret.push( S3_WELL_PLAYED );
-		ret.push( S4_OOPS );
-		ret.push( S5_ALL_YOU_GOT );
+		//ret.push( S4_OOPS );
+		//ret.push( S5_ALL_YOU_GOT );
 		ret.push( S6_HAHAHA );
 		ret.push( game.player.inFriendlyBattle ? S7_REVENGE : S8_CURSED );
 		
 		if( game.appVersion >= 2070 )
 		{
-			ret.push( S9_I_AFRAID );
+			//ret.push( S9_I_AFRAID );
 			ret.push( S10_DO_YOU_ANY );
 			ret.push( S11_NOVICE );
 			ret.push( S12_PRACTICE );
-			ret.push( S13_DISCONNECTED );
+			//ret.push( S13_DISCONNECTED );
 			ret.push( S14_PROFESSIONAL );
 			ret.push( S15_DONT_FORCE );
 			ret.push( S16_I_SEE_YOU_SMALL );
@@ -64,24 +64,24 @@ class StickerType
 		if( ratio < 0 )
 		{
 			ret.push( S0_LUCKKY );
-			ret.push( S4_OOPS );
+			//ret.push( S4_OOPS );
 			ret.push( S8_CURSED );
 			
 			if( game.appVersion >= 2070 )
 			{
-				ret.push( S9_I_AFRAID );
+				//ret.push( S9_I_AFRAID );
 				ret.push( S14_PROFESSIONAL );
 			}
 		}
 		else if( ratio > 0 )
 		{
 			ret.push( S2_THREAT );
-			ret.push( S5_ALL_YOU_GOT );
+			//ret.push( S5_ALL_YOU_GOT );
 			ret.push( S6_HAHAHA );
 			
 			if( game.appVersion >= 2070 )
 			{
-				ret.push( S9_I_AFRAID );
+				//ret.push( S9_I_AFRAID );
 				ret.push( S10_DO_YOU_ANY );
 				ret.push( S11_NOVICE );
 				ret.push( S12_PRACTICE );
@@ -103,20 +103,20 @@ class StickerType
 		{
 			case 0	:	getRandom([S6_HAHAHA, S12_PRACTICE, S16_I_SEE_YOU_SMALL]);
 			case 1	: 	getRandom([S1_THANKS, S3_WELL_PLAYED]);
-			case 2	: 	getRandom([S4_OOPS, S6_HAHAHA, S8_CURSED, S9_I_AFRAID, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
+			case 2	: 	getRandom([/*S4_OOPS, */S6_HAHAHA, S8_CURSED/*, S9_I_AFRAID*/, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
 			case 3	: 	getRandom([S1_THANKS, S12_PRACTICE]);
-			case 4	: 	getRandom([S2_THREAT, S3_WELL_PLAYED, S4_OOPS, S10_DO_YOU_ANY, S15_DONT_FORCE]);
+			case 4	: 	getRandom([S2_THREAT, S3_WELL_PLAYED, /*S4_OOPS, */S10_DO_YOU_ANY, S15_DONT_FORCE]);
 			case 5	: 	getRandom([S2_THREAT, S16_I_SEE_YOU_SMALL]);
-			case 6	: 	getRandom([S2_THREAT, S5_ALL_YOU_GOT, S14_PROFESSIONAL, S16_I_SEE_YOU_SMALL]);
+			case 6	: 	getRandom([S2_THREAT, /*S5_ALL_YOU_GOT, */S14_PROFESSIONAL, S16_I_SEE_YOU_SMALL]);
 			case 8	: 	getRandom([S2_THREAT, S6_HAHAHA, S12_PRACTICE, S15_DONT_FORCE]);
 			case 9	: 	getRandom([S2_THREAT, S6_HAHAHA, S12_PRACTICE, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
-			case 10	:	getRandom([S5_ALL_YOU_GOT, S6_HAHAHA, S14_PROFESSIONAL, S16_I_SEE_YOU_SMALL]);
-			case 11	:	getRandom([S0_LUCKKY, S4_OOPS, S6_HAHAHA, S9_I_AFRAID, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
-			case 12	:	getRandom([S4_OOPS, S6_HAHAHA, S10_DO_YOU_ANY, S11_NOVICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
-			case 13	:	getRandom([S4_OOPS, S10_DO_YOU_ANY, S15_DONT_FORCE]);
-			case 14	:	getRandom([S2_THREAT, S5_ALL_YOU_GOT, S6_HAHAHA, S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S16_I_SEE_YOU_SMALL]);
-			case 15	:	getRandom([S3_WELL_PLAYED, S6_HAHAHA, S8_CURSED, S9_I_AFRAID, S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
-			case 16	:	getRandom([S3_WELL_PLAYED, S6_HAHAHA, S8_CURSED, S9_I_AFRAID, S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
+			case 10	:	getRandom([/*S5_ALL_YOU_GOT, */S6_HAHAHA, S14_PROFESSIONAL, S16_I_SEE_YOU_SMALL]);
+			case 11	:	getRandom([S0_LUCKKY, /*S4_OOPS, */S6_HAHAHA/*, S9_I_AFRAID*/, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
+			case 12	:	getRandom([/*S4_OOPS, */S6_HAHAHA, S10_DO_YOU_ANY, S11_NOVICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
+			case 13	:	getRandom([/*S4_OOPS, */S10_DO_YOU_ANY, S15_DONT_FORCE]);
+			case 14	:	getRandom([S2_THREAT, /*S5_ALL_YOU_GOT, */S6_HAHAHA, S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S16_I_SEE_YOU_SMALL]);
+			case 15	:	getRandom([S3_WELL_PLAYED, S6_HAHAHA, S8_CURSED, /*S9_I_AFRAID, */S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
+			case 16	:	getRandom([S3_WELL_PLAYED, S6_HAHAHA, S8_CURSED, /*S9_I_AFRAID, */S10_DO_YOU_ANY, S11_NOVICE, S12_PRACTICE, S14_PROFESSIONAL, S15_DONT_FORCE, S16_I_SEE_YOU_SMALL]);
 			default :	S3_WELL_PLAYED;
 		}
 	}
