089cfd9032c6b1415f36b8fc1d9bedbd1c261fe5
ManJav <mansurjavadi@gmail.com>
2018-09-02 16:53:23 +0430

    increased damage-radius-max by lavel and imp-level balanced heavy power

--- source/src/com/gt/towers/calculators/DamageRadiusMaxCalculator.hx
+++ source/src/com/gt/towers/calculators/DamageRadiusMaxCalculator.hx
@@ -10,6 +10,6 @@ class DamageRadiusMaxCalculator extends BaseCalculator
 	public function new (game:Game) { super(game); BASE_VALUE = 200; }
 	override public function get( type:Int, level:Int = 1, improveLevel:Int = 1 ) : Float 
 	{
-		return BASE_VALUE ;
+		return BASE_VALUE + Math.log(level) * 10 + Math.log(improveLevel) * 20;
 	}
 }
