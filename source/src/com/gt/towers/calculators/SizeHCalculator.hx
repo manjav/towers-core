0b86e34a86b90224e4e7087ac55456198efe4300
gamadev20 <gamadev20@gmail.com.com>
2018-10-10 15:34:30 +0330

    added sizeH for units

--- source/src/com/gt/towers/calculators/SizeHCalculator.hx
+++ source/src/com/gt/towers/calculators/SizeHCalculator.hx
@@ -6,39 +6,23 @@ package com.gt.towers.calculators;
  */
 class SizeHCalculator extends BaseCalculator
 {
-	public function new (game:Game) { super(game); BASE_VALUE = 1; }
+	public function new (game:Game) { super(game); BASE_VALUE = 5; }
 	override public function get( type:Int, level:Int = 1, improveLevel:Int = 1 ) : Float 
 	{
-		var ret = switch ( type )
+		return switch ( type )
 		{
-			case	101	:	10	;
-			case	102	:	13;
-			case	103	:	6	;
-			case	104	:	9	;
+			case	101	:	20	;
+			case	102	:	16  ;
+			case	103	:	10	;
+			case	104	:	16	;
 			case	105	:	11	;
-			case	106	:	8	;
-			case	107	:	8	;
-			case	108	:	9	;
-			case	109	:	1	;
-			case	110	:	1	;
-			case	111	:	1	;
-			case	112	:	1	;
-			case	113	:	1	;
-			case	114	:	1	;
-			case	115	:	1	;
+			case	106	:	12	;
+			case	107	:	15	;
+			case	108	:	20	;
 			
-			case	151	:	1	;
-			case	152	:	1	;
-			case	153	:	1	;
-			case	154	:	1	;
-			case	155	:	1	;
-			case	156	:	1	;
-			case	157	:	1	;
-			case	158	:	1	;
-			case	159	:	1	;
+			case	201	:	30	;
 			
 			default: 1;
 		}
-		return ret * 0.1;
 	}
 }
