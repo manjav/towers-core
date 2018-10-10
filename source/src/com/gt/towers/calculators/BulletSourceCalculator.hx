a9c60fe4f1bf10bc884e1d4ab6e2e156e40b8864
reddice <barmaki.amin@gmail.com>
2018-10-10 15:32:13 +0330

    bullet source data entry phase 1

--- source/src/com/gt/towers/calculators/BulletSourceCalculator.hx
+++ source/src/com/gt/towers/calculators/BulletSourceCalculator.hx
@@ -15,14 +15,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(29 , -139);
+			case	102	:	new flash.geom.Point(34 , -206);
+			case	103	:	new flash.geom.Point(14 , -117);
+			case	104	:	new flash.geom.Point(15 , -146);
+			case	105	:	new flash.geom.Point(10 , -181);
+			case	106	:	new flash.geom.Point(27 , -93);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -32,14 +32,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(-33 , -130);
+			case	102	:	new flash.geom.Point(-54 , -190);
+			case	103	:	new flash.geom.Point(-52 , -105);
+			case	104	:	new flash.geom.Point(-61 , -128);
+			case	105	:	new flash.geom.Point(-67 , -161);
+			case	106	:	new flash.geom.Point(10 , -105);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -49,14 +49,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(-93 , -83);
+			case	102	:	new flash.geom.Point(-108 , -142);
+			case	103	:	new flash.geom.Point(-91 , -60);
+			case	104	:	new flash.geom.Point(-102 , -72);
+			case	105	:	new flash.geom.Point(-113 , -111);
+			case	106	:	new flash.geom.Point(11 , -103);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -66,14 +66,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(-88 , -22);
+			case	102	:	new flash.geom.Point(-108 , -69);
+			case	103	:	new flash.geom.Point(-69 , 8);
+			case	104	:	new flash.geom.Point(-84 , -10);
+			case	105	:	new flash.geom.Point(-94 , -43);
+			case	106	:	new flash.geom.Point(-28 , -92);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -83,14 +83,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(-21 , -11);
+			case	102	:	new flash.geom.Point(-39 , -26);
+			case	103	:	new flash.geom.Point(-22 , 39);
+			case	104	:	new flash.geom.Point(-18 , -16);
+			case	105	:	new flash.geom.Point(-14 , -6);
+			case	106	:	new flash.geom.Point(29 , -72);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -100,14 +100,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(88 , -22);
+			case	102	:	new flash.geom.Point(108 , -69);
+			case	103	:	new flash.geom.Point(69 , 8);
+			case	104	:	new flash.geom.Point(84 , -10);
+			case	105	:	new flash.geom.Point(94 , -43);
+			case	106	:	new flash.geom.Point(28 , -92);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -117,14 +117,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(93 , -83);
+			case	102	:	new flash.geom.Point(108 , -142);
+			case	103	:	new flash.geom.Point(91 , -60);
+			case	104	:	new flash.geom.Point(102 , -72);
+			case	105	:	new flash.geom.Point(113 , -111);
+			case	106	:	new flash.geom.Point(-11 , -103);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
@@ -134,14 +134,14 @@ class BulletSourceCalculator
 		{
 		return switch( type )
 		{
-			case	101	:	new flash.geom.Point(27, -206);
-			case	102	:	new flash.geom.Point(27, -206);
-			case	103	:	new flash.geom.Point(27, -206);
-			case	104	:	new flash.geom.Point(27, -206);
-			case	105	:	new flash.geom.Point(27, -206);
-			case	106	:	new flash.geom.Point(27, -206);
-			case	107	:	new flash.geom.Point(27, -206);
-			case	108	:	new flash.geom.Point(27, -206);
+			case	101	:	new flash.geom.Point(33 , -130);
+			case	102	:	new flash.geom.Point(54 , -190);
+			case	103	:	new flash.geom.Point(52 , -105);
+			case	104	:	new flash.geom.Point(61 , -128);
+			case	105	:	new flash.geom.Point(67 , -161);
+			case	106	:	new flash.geom.Point(-10 , -105);
+			case	107	:	new flash.geom.Point(0, 0);
+			case	108	:	new flash.geom.Point(0, 0);
 			case	201 :	new flash.geom.Point(27, -206);
 			default : 		new flash.geom.Point(27, -206);
 		}
