f7067775899b3268a177a865c980476e08a77b09
DESKTOP-VC622BS\Yoga <mansurjavadi@gmail.com>
2018-10-21 15:05:34 +0330

    fire position changed to point3

--- source/src/com/gt/towers/calculators/BulletFirePositionCalculator.hx
+++ source/src/com/gt/towers/calculators/BulletFirePositionCalculator.hx
@@ -1,163 +1,58 @@
 package com.gt.towers.calculators;
-import com.gt.towers.utils.CoreUtils;
+import com.gt.towers.utils.Point3;
 /**
  * ...
  * @author Mansour Djawadi
  */
-class BulletFirePositionCalculator extends BaseCalculator
+class BulletFirePositionCalculator
 {
-	public function new (game:Game) { super(game); BASE_VALUE = 0.0; }
-	override public function get( type:Int, level:Int = 1, improveLevel:Int = 1 ) : Float 
+	public function new () { }
+	static public function getPoint( type:Int, rotation:Int ) : Point3 
 	{
-		return switch ( type )
+		//var rad = CoreUtils.getRadString(rotation);
+		var weaponLen:Float = switch( type )
 		{
-			case	151	:	-1000	;
-			case	152	:	 1000	;
+			case	101	:	20;
+			case	102	:	20;
+			case	103	:	20;
+			case	104	:	20;
+			case	105	:	20;
+			case	106	:	20;
+			case	107	:	20;
+			case	108	:	20;
 			
-			default: BASE_VALUE;
+			case	201 :	20;
+			default : 		0;
 		}
+		
+		var x:Float = switch( type )
+		{
+			default : 		0;
+		}
+		
+		var y:Float = switch( type )
+		{
+			case	101	:	-30;
+			case	102	:	-30;
+			case	103	:	-30;
+			case	104	:	-30;
+			case	105	:	-30;
+			case	106	:	-30;
+			case	107	:	-30;
+			case	108	:	-30;
+			
+			case	152	:	 1000;
+			
+			case	201 :	-30;
+			default : 		 0;
+		}
+		
+		var z:Float = switch( type )
+		{
+			case	151	:	-1000;
+			default : 		 0;
+		}
+		
+		return new Point3(x + Math.cos(rotation) * weaponLen, y + Math.sin(rotation) * weaponLen, z);
 	}
-	
-	#if flash
-	static public function getPoint( type:Int, rotation:Int ) : flash.geom.Point 
-	{
-		var rad = CoreUtils.getRadString(rotation);
-		if( rad == "000" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(29 , -139);
-			case	102	:	new flash.geom.Point(34 , -206);
-			case	103	:	new flash.geom.Point(14 , -117);
-			case	104	:	new flash.geom.Point(15 , -146);
-			case	105	:	new flash.geom.Point(10 , -181);
-			case	106	:	new flash.geom.Point(27 , -93);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "045" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(-33 , -130);
-			case	102	:	new flash.geom.Point(-54 , -190);
-			case	103	:	new flash.geom.Point(-52 , -105);
-			case	104	:	new flash.geom.Point(-61 , -128);
-			case	105	:	new flash.geom.Point(-67 , -161);
-			case	106	:	new flash.geom.Point(10 , -105);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "090" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(-93 , -83);
-			case	102	:	new flash.geom.Point(-108 , -142);
-			case	103	:	new flash.geom.Point(-91 , -60);
-			case	104	:	new flash.geom.Point(-102 , -72);
-			case	105	:	new flash.geom.Point(-113 , -111);
-			case	106	:	new flash.geom.Point(11 , -103);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "135" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(-88 , -22);
-			case	102	:	new flash.geom.Point(-108 , -69);
-			case	103	:	new flash.geom.Point(-69 , 8);
-			case	104	:	new flash.geom.Point(-84 , -10);
-			case	105	:	new flash.geom.Point(-94 , -43);
-			case	106	:	new flash.geom.Point(-28 , -92);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "180" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(-21 , -11);
-			case	102	:	new flash.geom.Point(-39 , -26);
-			case	103	:	new flash.geom.Point(-22 , 39);
-			case	104	:	new flash.geom.Point(-18 , -16);
-			case	105	:	new flash.geom.Point(-14 , -6);
-			case	106	:	new flash.geom.Point(29 , -72);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "-35" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(88 , -22);
-			case	102	:	new flash.geom.Point(108 , -69);
-			case	103	:	new flash.geom.Point(69 , 8);
-			case	104	:	new flash.geom.Point(84 , -10);
-			case	105	:	new flash.geom.Point(94 , -43);
-			case	106	:	new flash.geom.Point(28 , -92);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "-90" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(93 , -83);
-			case	102	:	new flash.geom.Point(108 , -142);
-			case	103	:	new flash.geom.Point(91 , -60);
-			case	104	:	new flash.geom.Point(102 , -72);
-			case	105	:	new flash.geom.Point(113 , -111);
-			case	106	:	new flash.geom.Point(-11 , -103);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		
-		if( rad == "-45" )
-		{
-		return switch( type )
-		{
-			case	101	:	new flash.geom.Point(33 , -130);
-			case	102	:	new flash.geom.Point(54 , -190);
-			case	103	:	new flash.geom.Point(52 , -105);
-			case	104	:	new flash.geom.Point(61 , -128);
-			case	105	:	new flash.geom.Point(67 , -161);
-			case	106	:	new flash.geom.Point(-10 , -105);
-			case	107	:	new flash.geom.Point(0, 0);
-			case	108	:	new flash.geom.Point(0, 0);
-			case	201 :	new flash.geom.Point(27, -206);
-			default : 		new flash.geom.Point(27, -206);
-		}
-		}
-		return new flash.geom.Point();
-	}
-	#end
 }
---
+++ source/src/com/gt/towers/utils/Point3.hx
@@ -0,0 +1,19 @@
+package com.gt.towers.utils;
+
+/**
+ * ...
+ * @author Mansour Djawadi
+ */
+class Point3 
+{
+	public var x:Float;
+	public var y:Float;
+	public var z:Float;
+
+	public function new(x:Float, y:Float, z:Float = 0) 
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
