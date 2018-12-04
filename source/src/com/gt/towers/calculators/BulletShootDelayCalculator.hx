74f2eaebdc264cfe7aa45d3cda6e8d9bdfcee8f2
amin barmaki <barmaki.amin@gmail.com>
2018-12-04 19:33:02 +0330

    bullet shoot delay changed

--- source/src/com/gt/towers/calculators/BulletShootDelayCalculator.hx
+++ source/src/com/gt/towers/calculators/BulletShootDelayCalculator.hx
@@ -11,21 +11,21 @@ class BulletShootDelayCalculator  extends BaseCalculator
 	{
 		var ret = switch( type )
 		{
-			case	101	:	1.2	;
-			case	102	:	1.0	;
+			case	101	:	0.0	;
+			case	102	:	0.7	;
 			case	103	:	0.7	;
-			case	104	:	1.0	;
+			case	104	:	0.5	;
 			case	105	:	0.3	;
-			case	106	:	2.0	;
+			case	106	:	0.5	;
 			case	107	:	0.0	;
-			case	108	:	0.6	;
-			case	109	:	1.2	;
+			case	108	:	0.0	;
+			case	109	:	0.5	;
 			case	110	:	0.7	;
-			case	111	:	1.0	;
+			case	111	:	0.5	;
 			case	112	:	0.4 ;
 			case	113	:	0.4 ;
 			
-			case	201	:	0.8;
+			case	201	:	0.5;
 			
 			default	: BASE_VALUE;
 		}
