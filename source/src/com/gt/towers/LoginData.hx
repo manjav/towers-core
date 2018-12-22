6cd1ef1fa4b7b2eca438eeea39fbab92d9af870b
amin barmaki <barmaki.amin@gmail.com>
2018-12-22 18:17:03 +0330

    Login data hard currency rate changed

--- source/src/com/gt/towers/LoginData.hx
+++ source/src/com/gt/towers/LoginData.hx
@@ -11,7 +11,7 @@ import com.gt.towers.utils.maps.IntIntMap;
 class LoginData
 {
 	public static var coreSize:Int = 0;
-	public var coreVersion:String = "1200.1214005349";//do not change len.
+	public var coreVersion:String = "1200.1222175131";//do not change len.
 	public var noticeVersion:Int = 1200;
 	public var forceVersion:Int = 1200;
 	public var resources:IntIntMap;
@@ -30,7 +30,7 @@ class LoginData
 		resources.set(ResourceType.R1_XP, 0);
 		resources.set(ResourceType.R2_POINT, 0);
 		resources.set(ResourceType.R3_CURRENCY_SOFT, 100);
-		resources.set(ResourceType.R4_CURRENCY_HARD, 30);
+		resources.set(ResourceType.R4_CURRENCY_HARD, 100);
 		resources.set(CardTypes.C101, 1);
 		resources.set(CardTypes.C102, 1);
 		resources.set(CardTypes.C103, 1);
