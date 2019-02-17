0cd68465a131ba8e3809ba33c418ec77d9753501
Mansour <mansurjavadi@gmail.com>
2019-02-17 17:04:08 +0330

    removed only client art-ruls

---
+++ source/src/com/gt/towers/utils/Int3.hx
@@ -0,0 +1,19 @@
+package com.gt.towers.utils;
+
+/**
+ * ...
+ * @author 
+ */
+class Int3 extends Int2
+{
+	public var k:Int;
+	public function new(i:Int, j:Int, k:Int) 
+	{
+		super(i, j);
+		this.k = k;
+	}
+	/*public function toString() : String
+	{
+		return ("i:" + i + ", j:" + j + ")");
+	}*/
+}
